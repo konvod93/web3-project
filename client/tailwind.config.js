8a62f67a15a9fa9ef73fcf1e2002ac6d5922c5e0
Konstantin <konvod93@gmail.com>
2023-06-21 09:31:46 +0300

    created and stylized component Welcome, add changes in tailwind.config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"
   ],
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      variants: {
+        extend: {},
+      },
+    },
   },
   plugins: [],
 }
